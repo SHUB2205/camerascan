f6804448a75255b428d6e07b75d5c848a9945ad2
SHUB2205 <111409237+SHUB2205@users.noreply.github.com>
2023-02-14 01:57:59 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { RNCamera } from 'react-native-camera';
+
+render();{
+  return (
+    <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={{
+            flex: 1,
+            width: '100%',
+          }}
+          onGoogleVisionBarcodesDetected={this.barcodeRecognized}
+        >
+        </RNCamera>
+    </View>
+  );
+}
+
+barcodeRecognized = ({ barcodes }) => {
+  barcodes.forEach(barcode => console.warn(barcode.data))
+};
